5801e9dad0e7c4a15f5d86764172e3edf7f4b99f
ngwueche <wisdomngwueche@gmail.com>
2024-01-09 05:18:13 +0100

    Set up Dashboard Layout

---
+++ UserManagerApp.Models/ViewModels/UserToRegisterVM.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagerApp.Models.ViewModels
+{
+    public class UserToRegisterVM
+    {
+
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password")]
+        public string PasswordConfirmed { get; set; } = string.Empty;
+        [Required]
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
---
+++ UserManagerApp/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagerApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ManageUser()
+        {
+
+            return View();
+        }
+        public IActionResult ManageRole()
+        {
+
+            return View();
+        }
+    }
+}
