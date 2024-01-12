79c9b3e45821b135dba3026ace7d49d9d8298957
ngwueche <wisdomngwueche@gmail.com>
2024-01-12 23:59:26 +0100

    WORK IN PROGRESS

---
+++ UserManagerApp.Models/ViewModels/UserToLoginVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagerApp.Models.ViewModels
+{
+    public class UserToLoginVM
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+        public bool RememberMe { get; set; }
+    }
+}
--- UserManagerApp/Controllers/AccountController.cs
+++ UserManagerApp/Controllers/AccountController.cs
@@ -8,5 +8,14 @@ namespace UserManagerApp.Controllers
         {
             return View();
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return View();
+        }
     }
 }
