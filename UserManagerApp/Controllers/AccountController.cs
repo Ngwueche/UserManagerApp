0e4f4a4357f833c4d9b77eb2674f5dd91f3977a1
ngwueche <wisdomngwueche@gmail.com>
2024-01-13 23:57:07 +0100

    RegisterLogin_

--- UserManagerApp/Controllers/AccountController.cs
+++ UserManagerApp/Controllers/AccountController.cs
@@ -1,11 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserManagerApp.Data.Data;
+using UserManagerApp.Data.Repository.IRepository;
+using UserManagerApp.Models.Entities;
+using UserManagerApp.Models.ViewModels;
 
 namespace UserManagerApp.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Register()
+        //private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+        public AccountController(IUnitOfWork unitOfWork, ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+
+            _context = context;
+            _userManager = userManager;
+        }
+        public async IActionResult Register(UserToRegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var getUser = await _context.AppUsers.FindAsync(model.Email);
+                if (getUser != null)
+                {
+                    return Ok("Account already exist");
+                }
+                else
+                {
+                    var user = new ApplicationUser
+                    {
+                        Email = model.Email,
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        PhoneNumber = model.PhoneNumber,
+                        UserName = model.UserName,
+                    };
+                    var createUser = await _userManager.CreateAsync(user, model.Password);
+
+                }
+            }
             return View();
         }
 
